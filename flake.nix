adf7c0409a1e69c81c35dcd29506e87dee40f721
DavHau <d.hauer.it@gmail.com>
2026-04-27 14:32:12 +0000

    init: blueprint with treefmt-nix (nix, bash, python)

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  description = "distro";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+    blueprint.url = "github:numtide/blueprint";
+    blueprint.inputs.nixpkgs.follows = "nixpkgs";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs: inputs.blueprint { inherit inputs; };
+}
